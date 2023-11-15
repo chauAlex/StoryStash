39782487a6c2cbbec4e173c2d7cdacd8fb3a927c
xxiswajung <yoohj06@gmail.com>
2023-11-14 16:47:07 -0800

    unsolved issue about user must exist

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,12 @@
 class CommentsController < ApplicationController
-    # before_action :set_comment, only: %i[ show edit update destroy ]
-    # before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_comment, only: %i[ show edit update destroy ]
+    before_action :authenticate_user!, except: [:index, :show]
   
     def index
       @comments = Comment.all
     end
   
     def show
-        if params.key?(:user_id)
-            @user = User.find(param[:user_id])
-        end
         if params.key?(:article_id)
             @article = Article.find(params[:article_id])
         end
@@ -17,9 +14,6 @@ class CommentsController < ApplicationController
     end
   
     def new
-      if params.key?(:user_id)
-        @user = User.find(params[:user_id])
-      end
       @article = Article.find(params[:article_id])
       @comment = Comment.new
     end
@@ -28,33 +22,18 @@ class CommentsController < ApplicationController
     end
 
     def create
-        if params.key?(:user_id)
-            @article = Article.find(params[:article_id])
-            @user = User.find(params[:user_id])
-            @comment = Comment.new(comment_params)
-            respond_to do |format|
-              if @comment.save
-                format.html { redirect_to user_article_comment_url(@user, @article, @comment), notice: "Comment was successfully created." }
-                format.json { render :show, status: :created, location: @comment }
-              else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @comment.errors, status: :unprocessable_entity }
-              end
-            end
+        @article = Article.find(params[:article_id])
+        @comment = Comment.new(comment_params)
+    
+        respond_to do |format|
+          if @comment.save
+            format.html { redirect_to article_comment_url(@article, @comment), notice: "Comment was successfully created." }
+            format.json { render :show, status: :created, location: @comment }
           else
-            @article = Article.find(params[:article_id])
-            @comment = Comment.new(comment_params)
-      
-            respond_to do |format|
-              if @comment.save
-                format.html { redirect_to article_comment_url(@article, @comment), notice: "Comment was successfully created." }
-                format.json { render :show, status: :created, location: @comment }
-              else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @comment.errors, status: :unprocessable_entity }
-              end
-            end
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @comment.errors, status: :unprocessable_entity }
           end
+        end
     end
 
     def destroy
@@ -72,7 +51,7 @@ class CommentsController < ApplicationController
         end
     
         def comment_params
-          params.require(:comment).permit(:content, :article_id, :user_id)
+          params.require(:comment).permit(:content, :article_id)
         end
 
 end
