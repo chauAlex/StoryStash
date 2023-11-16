3d46ca2a598504bd9cb6c24aa9d84aace0bfaf83
Steven Choi <stychoi@gmail.com>
2023-11-16 15:51:04 -0800

    Revert "added basic test seed data"
    This reverts commit 91f79e2402af266cb19d4b42fffd9c679d6d6f94.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_14_233332) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_001654) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -40,6 +40,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_233332) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_transactions_on_article_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,8 +65,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_233332) do
   end
 
   add_foreign_key "articles", "users"
-  add_foreign_key "likes", "articles"
-  add_foreign_key "likes", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "articles"
+  add_foreign_key "likes", "users"
+  add_foreign_key "transactions", "articles"
+  add_foreign_key "transactions", "users"
 end
