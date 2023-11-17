3d8501406c9d18b05d83520070fe8430e737ca43
xxiswajung <yoohj06@gmail.com>
2023-11-16 16:24:40 -0800

    fix the show this article and pay button and show free article to unauthenticated user

--- db/migrate/20231116001654_add_price_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToArticle < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :price, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_001654) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_235042) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.integer "user_id", null: false
+    t.integer "price", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price", default: 0
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
